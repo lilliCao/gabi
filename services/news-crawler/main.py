5d5a23da349f1f51c1fb16989fde28519e8d4033
lilli <caothivananh98@gmail.com>
2020-12-10 23:47:35 +0100

    add schedule and dockerfile

--- services/news-crawler/main.py
+++ services/news-crawler/main.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+import w3lib.html
 import scrapy
 from kafka import KafkaProducer
 import os
@@ -8,7 +9,7 @@ from twisted.internet import reactor
 
 topic = os.environ['TOPIC']
 kafka = os.environ['KAFKA']
-next_pages = os.environ['NEXT_PAGES']
+pages = os.environ['PAGES']
 schedule = os.environ['SCHEDULE']
 
 producer = KafkaProducer(bootstrap_servers=[kafka],
@@ -18,13 +19,31 @@ producer = KafkaProducer(bootstrap_servers=[kafka],
 
 class NewsSpider(scrapy.Spider):
     name = "news"
-    start_urls = [
-        'https://www.dailyfx.com/market-news/articles'
-    ]
-    for i in range(next_pages):
+    start_urls = []
+    last_crawl = None
+    crawl_page = 0
+    for i in range(pages):
         start_urls.append('https://www.dailyfx.com/market-news/articles/{}'.format(i+1))
 
+    def get_text_of_the_article(self, response):
+        """
+        Get actual news text and send to kafka
+        :param response:
+        :return:
+        """
+        item = response.request.meta['item']
+        result = response.xpath('//div[@class="dfx-articleBody__content"]').extract_first()
+        if 'EURUSD' in result or 'EUR/USD' in result:
+            item['text'] = w3lib.html.remove_tags(result)
+            print('Publishing to kafka.....Time....{} Title ..... {}'.format(item['time'], item['title']))
+            producer.send(topic, str(item))
+
     def parse(self, response):
+        """
+        Parsing main page to get news title, date and url for further process
+        :param response:
+        :return:
+        """
         print('Crawling these pages {} at timestamp {}'.format(self.start_urls, datetime.now()))
         list_element = response.css('div.dfx-articleList')
         articles = list_element.css("a")
@@ -40,11 +59,23 @@ class NewsSpider(scrapy.Spider):
                 'time': time_reformat,
                 'url': url
             }
-            news.append(data)
+
+            if data not in news:
+                news.append(data)
+        # Sort news from newest to oldest
         news = sorted(news, key=lambda x: x['time'], reverse=True)
         print('Receiving {} articles Sample {}'.format(len(news), news[0]))
-        print('Publishing to kafka.................................')
-        producer.send(topic, news)
+        for item in news:
+            if NewsSpider.last_crawl is None or NewsSpider.crawl_page < pages:
+                print('This is the first crawling. Last crawl is {} or number of pages have been crawled {}'
+                      .format(NewsSpider.last_crawl, NewsSpider.crawl_page))
+            elif item['time'] < NewsSpider.last_crawl:
+                print('These news have already read the last time, skip all. Aborting crawling')
+                break
+            yield scrapy.Request(url=item['url'], callback=self.get_text_of_the_article, meta={'item': item})
+        NewsSpider.last_crawl = datetime.now()
+        NewsSpider.crawl_page = NewsSpider.crawl_page + 1
+        print('Updating last crawler to {}'.format(NewsSpider.last_crawl))
 
 
 def crawl_job():
